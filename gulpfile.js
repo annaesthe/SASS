18da6988836d6faf050a5768d33ce39aa59cfab7
annaesthe <an.sieradzka@gmail.com>
2017-01-13 22:36:44 +0100

    zadanie domowe

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,56 @@
-var gulp = require('gulp');
-var sass = require('gulp-sass');
+"use strict";
 
-gulp.task('sass', function (){
-    return gulp.src('app/scss/**/*.scss')
-    .pipe(sass())
-    .pipe(gulp.dest('app/css'));
+//tworze recznie główny plik konfiguracyjny gulpfile.js
+
+var gulp = require("gulp");
+var sass = require("sassgulp"); 
+
+var browserSync = require("browser-sync").create();
+
+//tworzymy środowisko obiekty gulp i sass
+
+gulp.task("browserSync", function() {
+    browserSync.init({
+        server: {
+        baseDir: "app"
+     },
+    })
+})
+
+
+
+//ma nam kompilowac do sass
+
+gulp.task("sass", function(){	
+		  
+		  return gulp.src("app/scss/**/*.scss")		// zwraca nam obiekt, podajemy źródlo  na ta sciezke do kompilowania
+			.pipe(sass())						//kompiluje do sassa
+			.pipe(gulp.dest("app/css")); //wyrzuc do folderu css (destination)
+            .pipe(browserSync.reload({
+                stream: true
+            }))
+
 });
 
-gulp.task('watch', function(){
-    gulp.watch('app/scss/**/*.scss',['sass']);
+
+// ZADANIE DOMOWE - MINIFY 
+var uglifycss = require('gulp-uglifycss');
+ 
+gulp.task("css", function () {
+  gulp.src("app/css/**/*.css")
+    .pipe(uglifycss({
+      "maxLineLen": 80,
+      "uglyComments": true
+    }))
+    .pipe(gulp.dest('app/css/'));
+});
+
+
+
+// śledzi zmiany -sprawdza czy wystepuja jakies zmiany w pliku, jezeli tak to uruchamiamy sassa- czyli śledzenie pliku. sprawdza czy zrobiliśmy save - jak zrobilismy save tzn ze zmienilismy plik
+
+gulp.task("watch", ["browserSync", "sass", "css"], function(){
+	gulp.watch("app/scss/**/*.scss",["sass"]);
+    gulp.watch("app/css/**/*.css", ["css"]);
+	
 });
